c32eb421d414a48d6ecb298e519b0a5a4030725b
Dennisbonke <dennisb@kabelfoon.net>
2015-01-15 10:15:49 +0100

    Week 1, Day 2. Now features a misbehaving, spastical screen that CAN give you epilepsi! Close by task manager, close button does not work yet.

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,13 +1,121 @@
 #include <windows.h>
 
-int CALLBACK
-WinMain(HINSTANCE hInstance,
-        HINSTANCE hPrevInstance,
-        LPSTR lpCmdLine,
-        int nCmdShow)
+LRESULT CALLBACK
+MainWindowCallback(HWND Window,
+                   UINT Message,
+                   WPARAM WParam,
+                   LPARAM LParam)
 {
-    MessageBoxA(0, "This is Handmade Hero.", "Handmade Hero",
-               MB_OK|MB_ICONINFORMATION);
+    LRESULT Result = 0;
+
+    switch(Message)
+    {
+        case WM_SIZE:
+        {
+            OutputDebugStringA("WM_SIZE\n");
+        } break;
+
+        case WM_DESTROY:
+        {
+            OutputDebugStringA("WM_DESTROY\n");
+        } break;
+
+        case WM_CLOSE:
+        {
+            OutputDebugStringA("WM_CLOSE\n");
+        } break;
+
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVATEAPP\n");
+        } break;
+
+        case WM_PAINT:
+        {
+            PAINTSTRUCT Paint;
+            HDC DeviceContext = BeginPaint(Window, &Paint);
+            int X = Paint.rcPaint.left;
+            int Y = Paint.rcPaint.top;
+            int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+            int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+            static DWORD Operation = WHITENESS;
+            PatBlt(DeviceContext, X, Y, Height, Width, Operation);
+            if(Operation == WHITENESS)
+            {
+                Operation = BLACKNESS;
+            }
+            else
+            {
+                Operation = WHITENESS;
+            }
+            EndPaint(Window, &Paint);
+        } break;
+
+        default:
+        {
+            Result = DefWindowProc(Window, Message, WParam, LParam);
+        } break;
+    }
+
+    return(Result);
+}
+int CALLBACK
+WinMain(HINSTANCE Instance,
+        HINSTANCE PrevInstance,
+        LPSTR CommandLine,
+        int ShowCode)
+{
+    WNDCLASS WindowClass = {};
+
+  // TODO(Dennis): Check if HREDRAW/VREDRAW/OWNDC still matter
+  WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+  WindowClass.lpfnWndProc = MainWindowCallback;
+  WindowClass.hInstance = Instance;
+//  WindowClass.hIcon;
+  WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+  if(RegisterClass(&WindowClass))
+  {
+      HWND WindowHandle =
+           CreateWindowEx(
+                0,
+                WindowClass.lpszClassName,
+                "Handmade Hero",
+                WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                0,
+                0,
+                Instance,
+                0);
+      if(WindowHandle)
+      {
+          MSG Message;
+          for(;;)
+          {
+              BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+              if(MessageResult > 0)
+              {
+                    TranslateMessage(&Message);
+                    DispatchMessage(&Message);
+              }
+              else
+              {
+                  break;
+              }
+          }
+      }
+      else
+      {
+          // TODO(Dennis): Logging
+      }
+  }
+  else
+  {
+      // TODO(Dennis): Logging
+  }
 
     return(0);
 }
