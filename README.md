dc386038254caf845cde2e138021750d34644b6a
Dennisbonke <dennisb@kabelfoon.net>
2015-01-21 20:16:31 +0100

    Halfway trough Day 6. See CHANGE.LOG for details
    Signed-off-by: Dennisbonke <dennisb@kabelfoon.net>

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Handmade Hero
 Handmade Hero is a ongoing project where I will be following the handmade hero tutorials by Casey Murrai.<br>
 <h2>Current version:</h2>
-0.0.1, build: 26.<br>
+0.0.1, build: 28.<br>
 <h2>Current day finished:</h2>
 Week 1, Day 5.<br>
 YES!!!! FINALLY SOMETHING ON THE SCREEN!!!!!
--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,5 +1,6 @@
 #include <windows.h>
 #include <stdint.h>
+#include <xinput.h>
 
 #define internal static
 #define local_persist static
@@ -35,7 +36,38 @@ struct win32_window_dimensions
 global_variable bool Running;
 global_variable win32_offscreen_buffer GlobalBackbuffer;
 
-win32_window_dimensions
+//NOTE(Dennis): XInputGetState
+#define X_INPUT_GET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_STATE *pState)
+typedef X_INPUT_GET_STATE(x_input_get_state);
+X_INPUT_GET_STATE(XInputGetStateStub)
+{
+    return(0);
+}
+global_variable x_input_get_state *XInputGetState_ = XInputGetStateStub;
+#define XInputSetState XInputSetState_
+
+//NOTE(Dennis): XInputSetState
+#define X_INPUT_SET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_VIBRATION *pVibration)
+typedef X_INPUT_SET_STATE(x_input_set_state);
+X_INPUT_SET_STATE(XInputSetStateStub)
+{
+    return(0);
+}
+global_variable x_input_set_state *XInputSetState_ = XInputSetStateStub;
+#define XInputGetState XInputGetState_
+
+internal void
+Win32LoadXInput(void)
+{
+    HMODULE XInputLibrary = LoadLibrary("xinput1_3.dll");
+    if(XInputLibrary)
+    {
+        XInputGetState = (x_input_get_state *)GetProcAddress(XInputLibrary, "XInputGetState");
+        XInputSetState = (x_input_set_state *)GetProcAddress(XInputLibrary, "XInputSetState");
+    }
+}
+
+internal win32_window_dimensions
 Win32GetWindowDimension(HWND Window)
 {
     win32_window_dimensions Result;
@@ -187,6 +219,8 @@ WinMain(HINSTANCE Instance,
         LPSTR CommandLine,
         int ShowCode)
 {
+  Win32LoadXInput();
+
   WNDCLASS WindowClass = {};
 
   Win32ResizeDIBSection(&GlobalBackbuffer, 1280, 720);
@@ -235,6 +269,45 @@ WinMain(HINSTANCE Instance,
                   DispatchMessageA(&Message);
               }
 
+              //TODO(Dennis): Should we poll this more frequently
+              for(DWORD ControllerIndex = 0;
+                  ControllerIndex < XUSER_MAX_COUNT;
+                  ++ControllerIndex)
+              {
+                  XINPUT_STATE ControllerState;
+                  if(XInputGetState(ControllerIndex, &ControllerState) == ERROR_SUCCESS)
+                  {
+                      //NOTE(Dennis): This controller is plugged in
+                      //TODO(Dennis): See if ControllerState.dwPacketNumber increments too rapidly
+                      XINPUT_GAMEPAD *Pad = &ControllerState.Gamepad;
+
+                      bool Up = (Pad->wButtons & XINPUT_GAMEPAD_DPAD_UP);
+                      bool Down = (Pad->wButtons & XINPUT_GAMEPAD_DPAD_DOWN);
+                      bool Left = (Pad->wButtons & XINPUT_GAMEPAD_DPAD_LEFT);
+                      bool Right = (Pad->wButtons & XINPUT_GAMEPAD_DPAD_RIGHT);
+                      bool Start = (Pad->wButtons & XINPUT_GAMEPAD_START);
+                      bool Back = (Pad->wButtons & XINPUT_GAMEPAD_BACK);
+                      bool LeftShoulder = (Pad->wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER);
+                      bool RightShoulder = (Pad->wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER);
+                      bool AButton = (Pad->wButtons & XINPUT_GAMEPAD_A);
+                      bool BButton = (Pad->wButtons & XINPUT_GAMEPAD_B);
+                      bool XButton = (Pad->wButtons & XINPUT_GAMEPAD_X);
+                      bool YButton = (Pad->wButtons & XINPUT_GAMEPAD_Y);
+
+                      int16 StickX = Pad->sThumbLX;
+                      int16 StickY = Pad->sThumbLY;
+
+                      if(AButton)
+                      {
+                          YOffset += 2;
+                      }
+                  }
+                  else
+                  {
+                      //NOTE(Dennis): The controller is not available
+                  }
+              }
+
               RenderWeirdGradient(GlobalBackbuffer, XOffset, YOffset);
 
               win32_window_dimensions Dimension = Win32GetWindowDimension(Window);
@@ -243,7 +316,6 @@ WinMain(HINSTANCE Instance,
                                          Dimension.Width, Dimension.Height);
 
               ++XOffset;
-              YOffset += 2;
           }
       }
       else
